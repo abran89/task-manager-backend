ffde4f84448d4f42ff38086bb9ea14044c6b1750
Abraham Contreras <abranz.contreras@gmail.com>
2024-10-10 10:44:16 -0300

    Set up Express server and establish database connection

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const { dbConnection } = require('./config/dbConnect');
 
 const app = express();
 
 app.use(express.json());
 
-app.listen(4000, ()=> {
+app.listen(4000, async ()=> {
     console.log('Server is running on port 4000')
+    await dbConnection()
 });
