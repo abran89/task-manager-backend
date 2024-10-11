56f45bc5a9bff1cb2d4bd4f0ef0a4133007220a9
Abraham Contreras <abranz.contreras@gmail.com>
2024-10-11 01:32:52 -0300

    fix: enable CORS for cross-origin requests

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const { dbConnection } = require('./config/dbConnect');
 const taskRoutes = require('./routes/taskRoutes');
 const Task = require('./models/taskModel');
@@ -7,12 +8,17 @@ const errorHandler = require('./middlewares/errorHandler');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/tasks', taskRoutes);
 
 app.use(errorHandler);
 
+/**
+ * Synchronize the Task model with the database
+ * @returns {Promise}
+ */
 Task.sync({force: false})
 
 const PORT = process.env.PORT || 3000;
